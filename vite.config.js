8ecf98780236492321fcf35dea063edaeafdf585
iruslanav <iruslanav@gmail.com>
2026-03-01 19:41:28 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bbdd/',
+  base: '/liveBBDD/',
   build: {
+    outDir: 'docs',
     rollupOptions: {
       output: {
         manualChunks: {
